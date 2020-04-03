0369c8226d3a760c250766274086446102c4fcbc
Boringboys <35089626+Boringboys@users.noreply.github.com>
2020-04-03 21:29:01 +0800

    Site updated: 2020-04-03 21:28:52

--- js/oni.js
+++ js/oni.js
@@ -2,7 +2,99 @@
 	var audio = $('#audio')[0]
 	var body = $('body')
 	var galMenu = $('.gal-menu')
-
+	//	播放器参数初始化
+	var musicPlayer = $('#musicplayer')[0]
+	var preSongBtn = $('#pre-song')
+	var nextSongBtn = $('#next-song')
+	var playOrPauseBtn = $('#play-or-pause')
+	var tmpCurrentTime = 0
+	var isMusicPlaying = false
+	var musicList
+	var musicNum = 0
+	var playingMusicNum = -1
+	//	获取歌曲列表
+	var xhrMusicList=new XMLHttpRequest()
+	xhrMusicList.onreadystatechange=function(){
+		if (xhrMusicList.readyState==4 && xhrMusicList.status==200){
+			var tmpobj=eval("("+xhrMusicList.response+")")
+			musicNum=tmpobj.size
+			musicList=tmpobj.musiclist
+		}
+	}
+	xhrMusicList.open("GET","https://boringboys-1254394685.cos.ap-shanghai.myqcloud.com/gal/music/musiclist.json","true");
+	xhrMusicList.send();
+	//	上一曲
+	preSongBtn.on('click',function(e){
+		console.log("上一首")
+		if(musicList){
+			if(isMusicPlaying){
+				playOrPauseBtn.click()
+			}
+			if(playingMusicNum==-1){
+				playingMusicNum=0
+			}else{
+				playingMusicNum-=1
+			}
+			if(playingMusicNum<0){
+				playingMusicNum=musicNum-1
+			}
+			$('#musicplayer').attr("src","https://boringboys-1254394685.cos.ap-shanghai.myqcloud.com/gal/music/"+musicList[playingMusicNum].FullName)
+			playOrPauseBtn.click()
+		}
+		$(".circle").removeClass("open")
+		$(".GalMenu").delay(400).hide(0)
+		audio.pause()
+		audio.currentTime = 0
+	})
+	//	下一曲
+	nextSongBtn.on('click',function(e){
+		console.log("下一首")
+		if(musicList){
+			if(isMusicPlaying){
+				playOrPauseBtn.click()
+			}
+			if(playingMusicNum==-1){
+				playingMusicNum=0
+			}else{
+				playingMusicNum+=1
+			}
+			if(playingMusicNum>=musicNum){
+				playingMusicNum=0
+			}
+			$('#musicplayer').attr("src","https://boringboys-1254394685.cos.ap-shanghai.myqcloud.com/gal/music/"+musicList[playingMusicNum].FullName)
+			playOrPauseBtn.click()
+		}
+		$(".circle").removeClass("open")
+		$(".GalMenu").delay(400).hide(0)
+		audio.pause()
+		audio.currentTime = 0
+	})
+	//	播放/暂停
+	playOrPauseBtn.on('click',function(e){
+		if(isMusicPlaying){
+			console.log("暂停")
+			musicPlayer.pause()
+			isMusicPlaying = false
+			playOrPauseBtn.text("播放")
+		}else{
+			console.log("播放")
+			musicPlayer.play()
+			isMusicPlaying = true
+			playOrPauseBtn.text("暂停")
+		}			
+		$(".circle").removeClass("open")
+		$(".GalMenu").delay(400).hide(0)
+		audio.pause()
+		audio.currentTime = 0
+	})
+	//	音乐结束事件
+	musicPlayer.onended = function(){
+		console.log("播放结束")
+		isMusicPlaying = false
+		playOrPauseBtn.text("播放")
+		nextSongBtn.click()
+	}
+	//////////////////////
 	body.on('mousedown', function (e) {
 		if(e.which !== 3 && $(e.target).parents('.gal-menu').length < 1) {
 			body.find('.gal-menu').stop(true, false).animate({
@@ -82,7 +174,9 @@
 			audio.currentTime = 0
 		} else {
 			$(".circle").addClass("open")
-			audio.play()
+			if(!isMusicPlaying){
+				audio.play()
+			}
 		}
 
 	})
